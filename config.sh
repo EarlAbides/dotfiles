25637d5a8a4815b6e458dc83a10ac6db310c4f85
Jeffrey Earl <jeffreyearl@hotmail.com>
2017-11-19 08:46:25 -0500

    Add config scripts

---
+++ config.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env zsh
+
+# -----------------------------------------------------------------------------
+# "THE BEER-WARE LICENSE" (Revision 42):
+# <bonecrusher@covenofchaos.com>  wrote this file.  As long as  you retain this
+# notice you can do whatever you want with this stuff.  If we meet some day and
+# you think this stuff is worth it, you can buy me a beer in return.
+#  > BoneCrusher <
+# -----------------------------------------------------------------------------
+
+mydir=${0:a:h}
+config_dir=${mydir}/../scripts
+
+[[ $1 ]] && { config_dir = $1 }
+
+configs=(
+    aliases
+    zsh
+    iterm2
+    istatmenus
+)
+
+for config in "${configs[@]}"
+do
+  if [[ -f ${config_dir}/configure_${config} ]]
+  echo 'configuring' "${config}"
+  ${mydir}/scripts/configure_${config} 
+done
